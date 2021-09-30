7461a00d672728538d4d416f4fa10b2699c6f500
Suneel Vanapalli <A30001344@agl.com.au>
2021-09-30 19:15:07 +1000

    initial setup

---
+++ my-website/docs/codeblocks/test1.jsx
@@ -0,0 +1,13 @@
+export default function HomepageFeatures() {
+    return (
+      <section className={styles.features}>
+        <div className="container">
+          <div className="row">
+            {FeatureList.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
+        </div>
+      </section>
+    );
+  }
---
+++ my-website/docs/docs/test1.md
@@ -0,0 +1,11 @@
+
+import CodeBlock from '@theme/CodeBlock';
+import MyComponentSourceTest from '!!raw-loader!../codeblocks/test1.jsx';
+import Partial from '../partials/_partial.mdx';
+
+<p>test</p>
+
+<CodeBlock className="language-jsx">{MyComponentSourceTest}</CodeBlock>
+
+<!-- 
+<Partial name="test1"></Partial> -->
